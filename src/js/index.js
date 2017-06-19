e0f7167e460627e7add68da5cc60f225ce9f868c
mengsai <you@example.com>
2017-06-19 21:32:40 +0800

    第4次同城帮

--- src/js/index.js
+++ src/js/index.js
@@ -14,6 +14,40 @@ $(function () {
         })
     }
     lun();
-
+$(".xiushouji").mouseover(function () {
+    $(".xiushouji").css("background","#00aa00");
+    $(".xiushoujidiv").css("display","block");
+})
+    $(".xiushouji").mouseout(function () {
+        $(".xiushouji").css("background","");
+        $(".xiushoujidiv").css("display","none");
+    })
+    $(".xiudiannao").mouseover(function () {
+        $(".xiudiannao").css("background","#00aa00");
+        $(".xiushoujidiv1").css("display","block");
+        
+    })
+    $(".xiudiannao").mouseout(function () {
+        $(".xiudiannao").css("background","");
+        $(".xiushoujidiv1").css("display","none");
+    })
+    $(".mai").mouseover(function () {
+        $(".mai").css("background","#00aa00");
+        $(".xiushoujidiv2").css("display","block");
+        
+    })
+    $(".mai").mouseout(function () {
+        $(".mai").css("background","");
+        $(".xiushoujidiv2").css("display","none");
+    })
+    $(".maishouji").mouseover(function () {
+        $(".maishouji").css("background","#00aa00");
+        $(".xiushoujidiv3").css("display","block");
+        
+    })
+    $(".maishouji").mouseout(function () {
+        $(".maishouji").css("background","");
+        $(".xiushoujidiv3").css("display","none");
+    })
 
 });
